1d821873a8271e6237fca41b8191bf964964c67c
Taraskiiin <taraskravets95@gmail.com>
2021-12-04 17:35:28 +0200

    (create): texture

---
+++ src/components/Cart/Cartblock.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cartblock: React.FC = () => {
+  return <div></div>;
+};
+
+export default Cartblock;
---
+++ src/components/List/Listbloc.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Listblock: React.FC = () => {
+  return <div></div>;
+};
+
+export default Listblock;
